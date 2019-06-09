7ca9050dcbe719ebd984a560240a29daeea7d1b0
eniallator <eniallatormc@gmail.com>
2019-06-09 14:41:25 +0100

    Added html/js template files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Web-Animation-Template
+
 My template repository for creating web animations
---
+++ index.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+ctx.fillStyle = 'black'
+ctx.strokeStyle = 'white'
+
+function run() {
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    // Animation code
+
+    requestAnimationFrame(run)
+}
+
+run()
