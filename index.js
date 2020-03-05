818d96a41cffd730d5acd5e85a9bdf504718ee71
eniallator <eniallatormc@gmail.com>
2020-03-05 18:21:25 +0000

    Implemented an aspect ratio that can be modified

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const aspectRatio = 16 / 9;
+
+const noScrollbarOffset = 3;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+if (canvas.width / canvas.height > aspectRatio) {
+  canvas.width -= canvas.width - canvas.height * aspectRatio;
+} else if (canvas.width / canvas.height < aspectRatio) {
+  canvas.height -= canvas.height - canvas.width * aspectRatio;
+}
+
+canvas.width -= noScrollbarOffset;
+canvas.height -= noScrollbarOffset;
+
 ctx.fillStyle = "black";
 ctx.strokeStyle = "white";
 
