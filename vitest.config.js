f058097e732349e8cda9dad6ecf17504f52b1b63
eniallator <eniallatormc@gmail.com>
2025-11-18 16:38:58 +0000

    Trying no node env for findissues/test

--- vitest.config.js
+++ vitest.config.js
@@ -3,9 +3,20 @@ import { defineConfig } from "vitest/config";
 const mode = process.env.NODE_ENV ?? "development";
 
 export default defineConfig({
-  test: {
-    passWithNoTests: true,
-    environment: "jsdom",
-    ...(mode === "production" ? { maxWorkers: 1, reporters: "verbose" } : {}),
-  },
+  test:
+    mode === "production"
+      ? {
+          watch: false,
+          isolate: true,
+          passWithNoTests: true,
+          environment: "jsdom",
+          fileParallelism: false,
+          reporters: "verbose",
+        }
+      : {
+          watch: false,
+          isolate: true,
+          passWithNoTests: true,
+          environment: "jsdom",
+        },
 });
